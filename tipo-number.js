9fd26663d15c70c8000923fc6658ee3302988a7e
Hello!!! Eu sou Fabiana Cunha!!! <107757832+ProfFabiana@users.noreply.github.com>
2025-03-26 18:18:49 +0000

    testes

---
+++ tipo-number.js
@@ -0,0 +1,16 @@
+// tipo number
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero * segundoNumero;
+console.log(operacaoMatematica)
+
+// Ponto Flutuante
+
+const pontoFlutuante = 3.3;
+
+const novoNumero = primeiroNumero / pontoFlutuante;
+
+console.log(novoNumero)
